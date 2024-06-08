9a279048aa26ea8fd10cfaada610bb42c9b4b92f
albert <albert.xalimow@gmail.com>
2024-06-08 21:46:28 +0500

    nest g res user/auth

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cp from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.use(cp());
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:3001'],
+    exposedHeaders: 'set-cookie',
+  });
+
   await app.listen(4200);
 }
 bootstrap();
